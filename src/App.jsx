5e41b6576f0065ec7e7ce503028094d8bbca2bcd
asraf ali <moha.asrafali@gmail.com>
2023-09-17 20:55:54 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl text-gray-600">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
